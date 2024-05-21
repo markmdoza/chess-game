87d9fb3689183679cc71c27f3d6b214a1b6e5494
markmdoza <marklmdoza@gmail.com>
2024-05-21 17:08:33 -0400

    added logic for chess pieces

---
+++ chess-game/src/app/chess-logic/pieces/bishop.ts
@@ -0,0 +1,18 @@
+import { Piece } from './piece';
+import { FENChar, Coords, Color } from '../models';
+
+export class Bishop extends Piece {
+  protected override _FENChar: FENChar;
+  protected override _directions: Coords[] = [
+    { x: 1, y: 1 },
+    { x: 1, y: -1 },
+    { x: -1, y: 1 },
+    { x: -1, y: -1 },
+  ];
+
+  constructor(private pieceColor: Color) {
+    super(pieceColor);
+    this._FENChar =
+      pieceColor === Color.White ? FENChar.WhiteBishop : FENChar.BlackBishop;
+  }
+}
